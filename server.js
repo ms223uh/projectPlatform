71ce8a5aa398b1668aff9729dcb324b80a07c727
ms223uh@student.lnu.se <ms223uh@student.lnu.se>
2016-04-18 13:50:30 +0200

    Can show data from .txt file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
+var fs = require("fs");
+var fileName = "foo.txt";
+var fileName2 = "bar.txt";
+
+
 
 //Index
 app.get('/', function(req, res) {
@@ -8,7 +13,22 @@ app.get('/', function(req, res) {
 });
 //About
 app.get("/projects", function (req, res) {
-    res.render("pages/projects");
+    
+    
+    var data = fs.readFileSync(fileName, "utf8");
+    console.log(data);
+    var data2 = fs.readFileSync(fileName2, "utf8");
+    console.log(data2);
+    
+   var project = [
+        { ID: data},
+        { ID: data2}
+    ];
+    res.render("pages/projects", {
+        project: project
+    });
+    
+    
 });
 //MyProjects
 app.get("/myprojects", function (req, res) {
@@ -22,13 +42,15 @@ app.get("/createprojects", function (req, res) {
 app.get("/messages", function (req, res) {
      var message = [
         { ID: 'Node', messages : 3 },
-        { name: 'EJS', messages : 5 },
-        { name: 'Express', messages : 10 }
+        { ID: 'EJS', messages : 5 },
+        { ID: 'Express', messages : 10 }
     ];
     res.render("pages/messages", {
         message: message
     });
 });
+
+
 
 app.listen(8080);
 console.log("listening on port 8080! GG WP");
